cd10c000134f3d07032812c8991decc01af273f3
Johannes Reuter <jre@alpha-q.de>
2013-03-31 19:51:31 +0200

    anpassung port-bestimmung für appfog

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express'),
     store  = require("./blox/store");
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.VCAP_APP_PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
