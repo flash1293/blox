95796aca3109f6cbe096e1d940c6c0c254df19a8
Johannes Reuter <jre@alpha-q.de>
2013-04-02 16:52:27 +0200

    bugfix - veraltete dependency entfernt

--- blox/gameengine.js
+++ blox/gameengine.js
@@ -17,7 +17,7 @@ Object.defineProperty(
 var saws = require('./saws');
 var builder = require('../public/src/Builder');
 var config = require('../public/config');
-var modController = require('./modcontroller');
+var modController = {};
 var contents = {
 	players: require('../public/assets/players.json'),
 	tools: require('../public/assets/tools.json'),
